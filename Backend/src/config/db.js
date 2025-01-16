8a062c277067a29a227a0745832224752c3365a1
Pravin Kumar <pravinmimrot648@gmail.com>
2025-01-17 00:44:53 +0530

    enable cors

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { MONGO_URI } = process.env;
 
 const connectToDB = ()=>{
-     const connection = mongoose.connect(`${MONGO_URI}/todo_app`)
+     const connection = mongoose.connect(MONGO_URI)
      return connection
 }
 
